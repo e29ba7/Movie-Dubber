7ff948fcc27b2e7a656dd3422bfdcc0b2dba9975
f09f9095 <f09f9095.tuta.io>
2022-11-08 17:57:45 -0500

    bug fixes and clean up

---
+++ src/encoder.py
@@ -0,0 +1,139 @@
+import os
+import re
+import subprocess
+
+from PyQt6.QtCore import QRunnable, pyqtSlot, pyqtSignal, QObject
+from PyQt6.QtWidgets import QMessageBox
+
+from utils import ErrorDialog
+
+
+class Encode_signals(QObject):
+    complete = pyqtSignal(bool)
+    progress = pyqtSignal(int)
+    max_progress = pyqtSignal(int)
+
+class Encode(QRunnable):
+    MAIN_ALIVE = True
+    def __init__(
+        self,
+        delay_text_box,
+        volume_text_box,
+        audio_ratio_text_box,
+        audio_text_box,
+        video_text_box,
+        movie,
+        output_text_box,
+        source_video
+        ):
+        super().__init__()
+        self.delay_text_box = delay_text_box
+        self.volume_text_box = volume_text_box
+        self.audio_ratio_text_box = audio_ratio_text_box
+        self.audio_text_box = audio_text_box
+        self.video_text_box = video_text_box
+        self.movie = movie
+        self.output_text_box = output_text_box
+        self.source_video = source_video
+        self.signals = Encode_signals()
+
+    @pyqtSlot()
+    def run(self):
+        try:
+            '''FFMPEG Command line options'''
+            cmd = [
+                'ffmpeg',
+                '-i', f'"{self.video_text_box.text()}"',
+                '-ss', f'"{self.delay_text_box.text()}ms"',
+                '-i', f'"{self.audio_text_box.text()}"',
+                '-filter_complex', f'''\
+                    "[0:a:0]volume={self.volume_text_box.text()}dB[vol];\
+                    [1:a]asplit=2[sc][mix];\
+                    [vol][sc]sidechaincompress=threshold=0.01:\
+                    ratio={self.audio_ratio_text_box.text()}:\
+                    mix=1:attack=1[comp];\
+                    [comp][mix]amix"''',
+                '-c:v', 'copy',
+                '-c:a', 'ac3',
+                '-b:a', '256k',
+                '-map', '0:v',
+                '-map', '0:a:0',
+                '-map', '1:a:0',
+                '-metadata', f'"title={self.movie}"',
+                '-metadata', f'"source={self.source_video}"',
+                '-metadata', f'"delay={self.delay_text_box.text()}"',
+                '-metadata', f'"attenuation={self.audio_ratio_text_box.text()}"',
+                '-metadata', f'"volume={self.volume_text_box.text()}dB"',
+                f'"{self.output_text_box.text()}/{self.movie} - [Dubbed].mkv"', 
+                '-y'
+                ]
+
+            '''Open FFMPEG subprocess'''
+            self.ffmpeg = subprocess.Popen(
+                ' '.join(cmd),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                encoding='UTF-8',
+                shell=True
+                )
+
+            '''Get FFMPEG subprocess output'''
+            self.times = [] # For check_and_kill_if_finished()
+            first_duration = True
+            for line in iter(self.ffmpeg.stdout.readline, ''):
+                print(line.strip())
+                if Encode.MAIN_ALIVE:
+                    if 'not recognized as an internal' in line.strip():
+                        ErrorDialog('Missing FFMPEG', 'You must have FFMPEG in your PATH, or in the Movie Dubber root directory')
+                        raise FileNotFoundError
+                    if 'Duration:' in line.strip() and first_duration:
+                        self.duration = re.search(r'\d{2}:\d{2}:\d{2}', line.strip())
+                        self.translated_duration = self.convert_time_to_ms(self.duration.group())
+                        self.signals.max_progress.emit(self.translated_duration)
+                    if 'time=' in line.strip():
+                        self.time = re.search(r'\d{2}:\d{2}:\d{2}', line.strip())
+                        self.translated_time = self.convert_time_to_ms(self.time.group())
+                        self.check_and_kill_if_finished(self.translated_time)
+                    if 'muxing overhead:' in line.strip():
+                        self.signals.progress.emit(self.translated_duration)
+                        self.signals.complete.emit(True)
+                        self.kill_process()
+                else:
+                    self.kill_process()
+        except FileNotFoundError: ...
+
+    #################
+    #|  FUNCTIONS  |#
+    #################
+
+    def error_message(self, title, text):
+        self.error = QMessageBox(parent=self, text=text)
+        self.error.setWindowTitle(title)
+        self.error.exec()
+
+    def convert_time_to_ms(self, time):
+        '''Convert XX:XX:XX time string format to milliseconds (int)'''
+        self.hour, self.minute, self.second = [int(i) for i in time.split(':')]
+        return (self.hour*3600 + self.minute*60 + self.second) * 1000
+
+    def check_and_kill_if_finished(self, time):
+        '''Check if encoding is finished by making a list and comparing
+        the last value to the previous one, then kill if they're same time
+        (Fixes FFMPEG issue)'''
+        self.times.append(time)
+        if len(self.times) > 3:
+            self.times.pop(0) # Restrict list to 3 entries
+            if self.times[-1] == self.times[-2]: # Kill thread if the last two output times were the same (due to FFMPEG loop error)
+                self.signals.progress.emit(self.translated_duration)
+                self.signals.complete.emit(False)
+                self.kill_process()
+            else: self.signals.progress.emit(time)
+        else: self.signals.progress.emit(time)
+
+    def kill_process(self):
+        '''Check what OS user running and kill running process'''
+        if os.name == 'nt':  # Windows systems
+            subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=self.ffmpeg.pid))
+        else: # Everything else
+            self.ffmpeg.kill()
+            self.ffmpeg.communicate()
---
+++ src/main_ui.py
@@ -0,0 +1,201 @@
+import re
+
+from PyQt6.QtCore import QRect, QRegularExpression, QThreadPool
+from PyQt6.QtGui import QRegularExpressionValidator
+from PyQt6.QtWidgets import QLineEdit, QPushButton, QProgressBar, QFileDialog
+
+import encoder
+from database import database_ui
+from media_editor import Editor
+from utils import DialogWindow, ErrorDialog
+
+
+class MainWindow(DialogWindow):
+    def __init__(self):
+        super().__init__('popcorn.ico', 'Movie Dubber', 271, 176)
+        '''Titlebar tooltip - Author'''
+        self.tb_title.setToolTip("Made by f09f9095")
+        '''Titlebar buttons'''
+        self.minimize_button = QPushButton('-', self.central_widget)
+        self.minimize_button.setGeometry(QRect(230, 4, 16, 16))
+        self.minimize_button.clicked.connect(self.showMinimized)
+        self.minimize_button.setStyleSheet("""
+            border: 1px solid #444444;
+            border-radius: 4px;"""
+            )
+        '''Output button'''
+        self.output_button = QPushButton('Output', self.central_widget)
+        self.output_button.setGeometry(QRect(10, 25, 61, 21))
+        self.output_button.setToolTip("Select file output location")
+        self.output_button.clicked.connect(self.output_file_location)
+        '''Output text box'''
+        self.output_text_box = QLineEdit(self.central_widget)
+        self.output_text_box.setGeometry(QRect(80, 25, 181, 21))
+        self.output_text_box.setToolTip("Select folder where output will be saved")
+        self.output_text_box.setPlaceholderText("Select Output Location")
+        '''Delay text box'''
+        self.delay_text_box = QLineEdit(self.central_widget)
+        self.delay_text_box.setGeometry(QRect(10, 55, 101, 21))
+        self.delay_text_box.setPlaceholderText("Delay (ms)")
+        self.delay_text_box.setToolTip("Enter movie delay in milliseconds")
+        self.delay_text_box.setValidator(QRegularExpressionValidator(QRegularExpression(r'\d{6}')))
+        '''Volume text box'''
+        self.volume_text_box = QLineEdit(self.central_widget)
+        self.volume_text_box.setGeometry(QRect(120, 55, 62, 21))
+        self.volume_text_box.setToolTip("""
+            <p>Enter volume change in dB (integer).
+            Can be negative. E.g. '5 or -3'</p>"""
+            )
+        self.volume_text_box.setPlaceholderText("Volume")
+        self.volume_text_box.setValidator(QRegularExpressionValidator(QRegularExpression(r'\d{2}')))
+        '''Audio ratio text box'''
+        self.audio_ratio_text_box = QLineEdit(self.central_widget)
+        self.audio_ratio_text_box.setGeometry(QRect(191, 55, 70, 21))
+        self.audio_ratio_text_box.setToolTip("""
+            <p>Enter ratio to attenuate
+            (While added audio is talking,
+            the movie track will temporarily drown out)
+            E.g. 2.5 (Range: 1 - 9.99)</p>"""
+            )
+        self.audio_ratio_text_box.setPlaceholderText("Ratio")
+        self.audio_ratio_text_box.setValidator(QRegularExpressionValidator(QRegularExpression(r'\d\.\d{2}')))
+        '''Video button'''
+        self.video_Button = QPushButton('Video File', self.central_widget)
+        self.video_Button.setGeometry(QRect(10, 85, 61, 21))
+        self.video_Button.setToolTip("Select video input")
+        self.video_Button.clicked.connect(self.get_video_file)
+        '''Video text box'''
+        self.video_text_box = QLineEdit(self.central_widget)
+        self.video_text_box.setGeometry(QRect(80, 86, 181, 21))
+        self.video_text_box.setToolTip("Select video input")
+        self.video_text_box.setPlaceholderText('Select Video File')
+        '''Audio button'''
+        self.audio_button = QPushButton('Audio File', self.central_widget)
+        self.audio_button.setGeometry(QRect(10, 115, 61, 21))
+        self.audio_button.setToolTip("Select audio input to overlay")
+        self.audio_button.clicked.connect(self.get_audio_file)
+        '''Audio text box'''
+        self.audio_text_box = QLineEdit(self.central_widget)
+        self.audio_text_box.setGeometry(QRect(80, 115, 181, 21))
+        self.audio_text_box.setToolTip("Select audio input to overlay")
+        self.audio_text_box.setPlaceholderText('Select Audio Track')
+        '''Load button'''
+        self.load_button = QPushButton('Load', self.central_widget)
+        self.load_button.setGeometry(QRect(10, 145, 50, 21))
+        self.load_button.setToolTip("Load sync information from database.")
+        self.load_button.clicked.connect(self.open_database)
+        '''Encode button'''
+        self.encode_button = QPushButton('Encode', self.central_widget)
+        self.encode_button.setGeometry(QRect(65, 145, 56, 21))
+        self.encode_button.setToolTip("Begin encoding process")
+        self.encode_button.clicked.connect(self.check_inputs_and_encode)
+        '''Editor button (Editor is not enabled)'''
+        self.editor_button = QPushButton('E', self.central_widget)
+        self.editor_button.setGeometry(QRect(207, 4, 16 ,16))
+        self.editor_button.setToolTip("The editor isn't ready yet.")
+        self.editor_button.setDisabled(True)
+        self.editor_button.setDefault(True)
+        self.editor_button.hide()
+        # self.editor_button.clicked.connect(self.start_editor)
+        '''Progress bar'''
+        self.progress_bar = QProgressBar(self.central_widget)
+        self.progress_bar.setGeometry(QRect(130, 145, 131, 21))
+        self.progress_bar.setMinimum(0)
+        '''Run'''
+        self.show()
+
+    #################
+    #|  FUNCTIONS  |#
+    #################
+
+    def get_video_file(self):
+        self.video_file = QFileDialog.getOpenFileName(
+            parent=self,
+            caption='Select Video File',
+            filter='Video files (*.mkv *.mp4 *.avi *.mov )'
+            )
+        self.video_text_box.setText(self.video_file[0])
+
+    def get_audio_file(self):
+        self.audio_file = QFileDialog.getOpenFileName(
+            parent=self,
+            caption='Select Audio File',
+            filter='Audio files (*.mp3 *.m4a)'
+            )
+        self.audio_text_box.setText(self.audio_file[0])
+
+    def output_file_location(self):
+        self.save_loc = QFileDialog.getExistingDirectory(
+            parent=self,
+            caption='Select save location'
+            )
+        self.output_text_box.setText(self.save_loc)
+
+    def open_database(self):
+        try:
+            self.data = database_ui.Ui_Database().data
+            self.movie = self.data[0]
+            self.delay_text_box.setText(str(self.data[3]))
+            self.audio_ratio_text_box.setText(str(self.data[4]))
+            self.volume_text_box.setText(str(self.data[5]))
+        except IndexError: ...
+
+    def check_inputs_and_encode(self):
+        try:
+            self.disable_encode_button()
+            '''Check Delay, Volume, and Ratio text boxes'''
+            if "/" not in self.output_text_box.text():
+                ErrorDialog('Output location error', 'You must select an output location folder.')
+                raise ValueError
+            if not re.match(r'\d', self.delay_text_box.text()):
+                ErrorDialog('Delay error', 'You must provide a delay in milliseconds')
+                raise ValueError
+            if not re.match(r'\d', self.volume_text_box.text()):
+                self.volume_text_box.setText('0')
+            if not re.match(r'\d', self.audio_ratio_text_box.text()):
+                self.audio_ratio_text_box.setText('1')
+            if "/" in self.video_text_box.text():
+                self.source_video = re.split("/", self.video_text_box.text())[-1]
+            else:
+                ErrorDialog('Video source error', 'You must select a video file')
+                raise ValueError
+            if "/" not in self.audio_text_box.text():
+                ErrorDialog('Audio source error', 'You must select an audio track')
+                raise ValueError
+            '''Threads'''
+            self.progress_bar.setValue(0)
+            self.pool = QThreadPool()
+            self.runnable = encoder.Encode(
+                self.delay_text_box,
+                self.volume_text_box,
+                self.audio_ratio_text_box,
+                self.audio_text_box,
+                self.video_text_box,
+                self.movie,
+                self.output_text_box,
+                self.source_video
+                )
+            self.runnable.signals.max_progress.connect(self.set_progress_max)
+            self.runnable.signals.progress.connect(self.set_current_progress)
+            self.runnable.signals.complete.connect(self.enable_encode_button)
+            self.pool.start(self.runnable)
+        except ValueError: self.enable_encode_button()
+
+    def set_progress_max(self, max_progress):
+        self.progress_bar.setMaximum(max_progress)
+
+    def set_current_progress(self, current_progress):
+        self.progress_bar.setValue(current_progress)
+
+    def enable_encode_button(self):
+        self.encode_button.setEnabled(True)
+
+    def disable_encode_button(self):
+        self.encode_button.setDisabled(True)
+
+    def start_editor(self):
+        self.editor = Editor(self.video_text_box.text(), self.audio_text_box.text())
+
+    '''Executes on program end'''
+    def closeEvent(self, event):
+        encoder.Encode.MAIN_ALIVE = False # Kill running ffmpeg process
